d063861186adb703607b9ba191f07df9e7dc59fa
Krishanna Williams <krishannaw@gmail.com>
2023-04-30 17:17:05 -0500

    hard

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+// Remove the 'main' element
+const main = document.querySelector('#main');
+main.remove();
+
+// Create a new 'h1' element
+const newHeader = document.createElement('h1');
+
+// Set the 'id' attribute of the 'h1' element to 'victory'
+newHeader.id = 'victory';
+
+// Set the text of the 'h1' element
+newHeader.textContent = 'YOUR-NAME is the champion'; // replace YOUR-NAME with your name
+
+// Append the 'h1' element to the 'body' element
+document.body.appendChild(newHeader);
+// Wait for the DOM to load
